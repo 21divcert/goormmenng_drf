489da6a1ae76e7c3fccb60f70b63a652fed83419
hiruca3220 <hiruca3220@daum.net>
2024-11-01 09:33:51 +0900

    ㅁㄴㄱㄹㅁㄴㄹ

--- barameong/urls.py
+++ barameong/urls.py
@@ -1,9 +1,15 @@
 from django.urls import path
 from .views import matchingAIAPIView, CompleteReservationAPIView, FindDogAPIView, DogListAPIView
 
+from django.http import JsonResponse
+
+def ping(request):
+    return JsonResponse({"message": "200"}, status=200)
+
 urlpatterns = [
     path('match/', matchingAIAPIView.as_view(), name='match'),
     path('complete/', CompleteReservationAPIView.as_view(), name='reservation'),
     path('find/', FindDogAPIView.as_view(), name='find'),
     path('dog/', DogListAPIView.as_view(), name='dog'),
+    path('api/ping/', views.ping, name='ping'),
 ]
